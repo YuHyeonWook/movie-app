9a31fef1e0ffff87045e7a55aa007b00f94dc015
DESKTOP-C2KV1NR\유현욱 <yho7955@naver.com>
2024-03-08 22:04:46 +0900

    feat: headline 기능 구현

---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+/*
+ * main page
+ */
+
+import { Component } from "../core/wook";
+
+export default class Home extends Component {
+  render() {
+    this.el.innerHTML = `
+        <h1> hello !!!!!1</h1>`;
+  }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,4 @@
+import { Component, createRouter } from "../core/wook";
+import Home from "./Home";
+
+export default createRouter([{ path: "#/", component: Home }]);
